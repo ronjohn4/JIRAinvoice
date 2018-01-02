1033f7880174a1ae938e3ab3c8469d88574aa1c7
Ron Johnson <ronjohn4@gmail.com>
2018-01-02 16:42:02 -0700

    Several small typo changes

---
+++ JIRAhandler.py
@@ -0,0 +1,32 @@
+import requests
+
+class JIRAhandler():
+    _JiraSession = None
+    _JiraBaseUrl = None
+
+    def __init__(self, JiraBaseUrl):
+        self._JiraSession = requests.session()  # NOT a Flask session
+        self._JiraBaseUrl = JiraBaseUrl
+        self._JiraSession.auth = None
+
+    # authenticate the specified auth against JIRA
+    def auth(self, s, auth):
+        self._JiraSession.auth = auth
+
+        r = self._JiraSession.get(self._JiraBaseUrl + '/rest/auth/1/session')
+        if r.status_code != 200:
+            self._JiraSession.auth = None
+            s['isAuthenticated'] = False
+        else:
+            s['isAuthenticated'] = True
+        return r.status_code == 200
+
+    def isAuth(self):
+        return self._JiraSession.auth != None
+
+    def logout(self, s):
+        self._JiraSession.auth = None
+        s['isAuthenticated'] = False
+
+
+
