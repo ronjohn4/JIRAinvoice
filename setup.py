3f3bd57f85925d25dba762e267358a3e22b64e0b
Ron Johnson <ronjohn4@gmail.com>
2018-01-03 17:18:50 -0700

    Clean up setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
    description='A useful module',
    author='MRon Johnson',
    author_email='ronjohn4@gmail.com',
-   packages=['JIRAProjectInvoice'],  #same as name
+   packages=['JIRAProjectInvoice', 'JIRAhandler'],  #same as name
    install_requires=[
         'certifi>=2017.7.27.1',
         'chardet>=3.0.4',
